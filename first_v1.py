a8f96eac63814f186d85c4e9943f6225403abd0b
sivasuribabu <sivasuribabupenkey@gmail.com>
2023-03-04 11:15:33 +0530

    another logic updated

--- first_v1.py
+++ first_v1.py
@@ -1,10 +1,27 @@
-hungry = input("are you hungry ..? :")
-if hungry == "yes":
-    print("Go and eat samosa")
-else:
-    print("Go and finish your home works")
+# hungry = input("are you hungry ..? :")
+# if hungry == "yes":
+#     print("Go and eat samosa")
+# else:
+#     print("Go and finish your home works")
 
 
-print("Hello World")
+# print("Hello World")
 
-This line is update in  online
+# print("This line is update in  online")
+
+
+# hrs = int(input("Enter how many hours you worked : "))
+# rate =int(input("Enter how much is for an hour :"))
+# pay = hrs*rate
+
+# print("Your total pay is : ", pay)
+# print("Have a great day and safe day")
+
+
+hrs = input("Enter a hours :")
+rate = input("Enter rate per Hour :")
+hrs = float(hrs)
+rate=float(rate)
+pay = hrs* rate
+print("Your total pay is : ", pay)
+
